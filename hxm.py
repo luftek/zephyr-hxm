ce3981bbbdcab406af8e06aaf36be35636c87b3d
luftek <stegu13@gmail.com>
2018-07-02 11:35:30 +0200

    PYTHON 2 -> 3 update
    - print is in braces, sys.stderr
    - correct type error

--- hxm.py
+++ hxm.py
@@ -4,6 +4,11 @@ import sys
 import serial
 import struct
 
+def xrange(x):
+
+    return iter(range(x))
+
+# ser = serial.Serial('COM9', 115200, timeout=1)
 ser = serial.Serial('/dev/tty.HXM011522-BluetoothSeri', 115200, timeout=1)
 
 async = False
@@ -15,112 +20,116 @@ while True:
 	d = ser.read()
 	if d != stx:
 		if not async:
-			print >>sys.stderr, "Not synched"
+			print("Not synched", file=sys.stderr)
 			async = True
 		continue
 	async = False
 	# 60 bytes total in message
-	print "Found message"
+	print("Found message")
 	type = ser.read()	# Msg ID
 	if type != rate:
-		print >>sys.stderr, "Unknown message type"
+		print("Unknown message type", file=sys.stderr)
 	dlc = ser.read()	# DLC
 	len, = struct.unpack("<B", dlc)
 	if len != 55:
-		print >>sys.stderr, "Bad DLC"
+		print("Bad DLC", file=sys.stderr)
 	payload = ser.read(len)
+	print(payload)
 	crc, = struct.unpack("<B", ser.read())
 	end, = struct.unpack("<B", ser.read())
 	sum = 0
-	print "L: " + str(len)
+	print("L: " + str(len))
 	for i in xrange(len):
-		b, = struct.unpack("<B", payload[i])
-		#print "Data: 0x%02x" % b
+		print(payload[i])
+		# b, = struct.unpack("<B", payload[i])
+		b = payload[i]
+		#print("Data: 0x%02x" % b)
 		sum = (sum ^ b) & 0xff
 		for j in xrange(8):
 			if sum & 0x01:
 				sum = (sum >> 1) ^ 0x8c
 			else:
 				sum = (sum >> 1)
-	#print "CRC:  0x%02x" % crc
+	#print("CRC:  0x%02x" % crc)
 	if crc != sum:
-		print >>sys.stderr, "Bad CRC: " + str(sum) + " is not " + str(crc)
+		print("Bad CRC: " + str(sum) + " is not " + str(crc), file=sys.stderr)
 	else:
-		print "CRC validated!"
+		print("CRC validated!")
 	if end != 0x03:
-		print >>sys.stderr, "Bad ETX"
+		print("Bad ETX", file=sys.stderr)
 	fid, fiv, hid, hiv, batt, hr, hbn, hbts1, hbts2, hbts3, hbts4, hbts5, hbts6, hbts7, hbts8, hbts9, hbts10, hbts11, hbts12, hbts13, hbts14, hbts15, distance, speed, strides = struct.unpack("<H2sH2sBBB15H6xHHB3x", payload)
-	print "Firmware: 9500.%04d.V%2s" % (fid, fiv)
-	print "Hardware: 9800.%04d.V%2s" % (hid, hiv)
-	print "Battery: %3d%%" % batt
-	print "Heart Rate: %3d bpm" % hr
-	print "Heart Beat Number: %d" % hbn
-	print "Heart Beat Timestamp #1 (Newest): %d ms" % hbts1
+	print("Firmware: 9500.%04d.V%2s" % (fid, fiv))
+	print("Hardware: 9800.%04d.V%2s" % (hid, hiv))
+	print("Battery: %3d%%" % batt)
+	print("Heart Rate: %3d bpm" % hr)
+	print("Heart Beat Number: %d" % hbn)
+	print("Heart Beat Timestamp #1 (Newest): %d ms" % hbts1)
 	#for i in xrange(14):
-	#	print "Heart Beat Timestamp #%d: %d ms" % (i+2, byte)
-	print "Distance: %.3f m" % (float(distance)/16)
-	print "Instantaneous Speed: %.3f m/s" % (float(speed)/256)
-	print "Strides: %d" % strides
+	#	print("Heart Beat Timestamp #%d: %d ms" % (i+2, byte))
+	print("Distance: %.3f m" % (float(distance)/16))
+	print("Instantaneous Speed: %.3f m/s" % (float(speed)/256))
+	print("Strides: %d" % strides)
 	continue
 	data = ser.read(2)	# Firmware ID
 	fid, = struct.unpack("<H", data)
-	#print "Firmware ID: 0x%02x" % fid
+	#print("Firmware ID: 0x%02x" % fid)
 	data = ser.read(2)	# Firmware Version
 	fiv, = struct.unpack("<2s", data)
-	#print "Firmware Version: 0x%02x" % fiv
+	#print("Firmware Version: 0x%02x" % fiv)
 	data = ser.read(2)	# Hardware ID
 	hid, = struct.unpack("<H", data)
-	#print "Hardware ID: 0x%02x" % hid
+	#print("Hardware ID: 0x%02x" % hid)
 	data = ser.read(2)	# Hardware Version
 	hiv, = struct.unpack("<2s", data)
-	#print "Hardware Version: 0x%02x" % hiv
-	print "Firmware: 9500.%04d.V%2s" % (fid, fiv)
-	print "Hardware: 9800.%04d.V%2s" % (hid, hiv)
+	#print("Hardware Version: 0x%02x" % hiv)
+	print("Firmware: 9500.%04d.V%2s" % (fid, fiv))
+	print("Hardware: 9800.%04d.V%2s" % (hid, hiv))
 	data = ser.read()	# Battery Charge Indicator
 	byte, = struct.unpack("<B", data)
-	print "Battery: %3d%%" % byte
+	print("Battery: %3d%%" % byte)
 	data = ser.read()	# Heart Rate
 	byte, = struct.unpack("<B", data)
-	print "Heart Rate: %3d bpm" % byte
+	print("Heart Rate: %3d bpm" % byte)
 	data = ser.read()	# Heart Beat Number
 	byte, = struct.unpack("<B", data)
-	print "Heart Beat Number: %d" % byte
+	print("Heart Beat Number: %d" % byte)
 	data = ser.read(2)	# Heart Beat Timestamp #1 (Newest)
 	byte, = struct.unpack("<H", data)
-	print "Heart Beat Timestamp #1 (Newest): %d ms" % byte
+	print("Heart Beat Timestamp #1 (Newest): %d ms" % byte)
 	for i in xrange(14):
 		data = ser.read(2)	# Heart Beat Timestamp #x
 		byte, = struct.unpack("<H", data)
-		print "Heart Beat Timestamp #%d: %d ms" % (i+2, byte)
+		print("Heart Beat Timestamp #%d: %d ms" % (i+2, byte))
 	data = ser.read(2)	# Reserved
 	byte, = struct.unpack("<H", data)
-	print "Reserved: %d" % byte
+	print("Reserved: %d" % byte)
 	data = ser.read(2)	# Reserved
 	byte, = struct.unpack("<H", data)
-	print "Reserved: %d" % byte
+	print("Reserved: %d" % byte)
 	data = ser.read(2)	# Reserved
 	byte, = struct.unpack("<H", data)
-	print "Reserved: %d" % byte
+	print("Reserved: %d" % byte)
 	data = ser.read(2)	# Distance
 	byte, = struct.unpack("<H", data)
-	print "Distance: %.3f m" % (float(byte)/16)
+	print("Distance: %.3f m" % (float(byte)/16))
 	data = ser.read(2)	# Instantaneous Speed
 	byte, = struct.unpack("<H", data)
-	print "Instantaneous Speed: %.3f m/s" % (float(byte)/256)
+	print("Instantaneous Speed: %.3f m/s" % (float(byte)/256))
 	data = ser.read()	# Strides
 	byte, = struct.unpack("<B", data)
-	print "Strides: %d" % byte
+	print("Strides: %d" % byte)
 	data = ser.read()	# Reserved
 	byte, = struct.unpack("<B", data)
-	print "Reserved: %d" % byte
+	print("Reserved: %d" % byte)
 	data = ser.read(2)	# Reserved
 	byte, = struct.unpack("<H", data)
-	print "Reserved: %d" % byte
+	print("Reserved: %d" % byte)
 	data = ser.read()	# CRC
 	byte, = struct.unpack("<B", data)
-	print "CRC: %d" % byte
+	print("CRC: %d" % byte)
 	end = ser.read()
 	while end != etx:
-		print "Extra"
+		print("Extra")
 		end = ser.read()
-	#print struct.unpack("<B", d)
+	#print(struct.unpack("<B", d))
+	
